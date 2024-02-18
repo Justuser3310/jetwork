fa0febe93b4cde466dc031d2c63e173d3332e969
none <none>
2024-02-18 19:44:57 +0300

    up

---
+++ db.py
@@ -0,0 +1,20 @@
+import os
+import json
+
+if not os.path.exists('config.json'):
+	db = {}
+	js = json.dumps(db, indent=2)
+	with open("config.json", "w") as outfile:
+		outfile.write(js)
+	print('Created new config.json')
+
+
+def read(file = 'config.json'):
+	with open(file, "r", encoding="utf-8") as openfile:
+		db = json.load(openfile)
+	return db
+
+def write(db, file = 'config.json'):
+	js = json.dumps(db, indent=2, ensure_ascii=False)
+	with open(file, "w", encoding="utf-8") as outfile:
+		outfile.write(js)
