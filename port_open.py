b559b7b65993b25caf4e497ecf23f619e393d6fd
justuser <you@example.com>
2024-06-18 10:33:52 +0300

    Перевод на публичный адрес

--- port_open.py
+++ port_open.py
@@ -2,15 +2,15 @@ from os import system, name
 from network import port_gen
 
 port = input("Введите порт: ")
-dest = port_gen(7000, 9000)
+dest = port_gen(25300, 25500)
 
 print(f"\nУдалённый ПОРТ: {dest}")
 print("\n[!] Не закрывайте это окно [!]\n")
 
 if name == "posix":
-	system(f"./bore local {port} --to jetwork.404.mn --port {dest}")
+	system(f"./bore local {port} --to bore.pub --port {dest}")
 elif name == "nt":
-	system(f"bore.exe local {port} --to jetwork.404.mn --port {dest}")
+	system(f"bore.exe local {port} --to bore.pub --port {dest}")
 else:
 	print("Увы, вероятно Ваша ОС не поддерживается.")
 	print("Завершение работы...")
