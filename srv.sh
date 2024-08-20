59aeeaac3340224ba9e93b42e0c6be597f6d2061
justuser <you@example.com>
2024-08-20 16:50:08 +0300

    Исправление бага

--- srv.sh
+++ srv.sh
@@ -1,8 +1,9 @@
 while true
 do
 killall python
-sleep 2s
+sleep 1s
 killall python #Чтоб наверняка
+sleep 60s # Ждём, чтобы не было ошибки при перезапуске "данный порт уже используется"
 
 nohup python srv_main.py &
 sleep 6h
